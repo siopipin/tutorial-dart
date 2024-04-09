fc62d59372ced1b2597a5137e0de4db296408883
siopipin <siojurnalispipin@hotmail.com>
2024-04-09 12:37:17 +0700

    add struktur program dasar

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,5 +1,14 @@
+import 'package:dart_basic/M1.1.struktur-program/library.dart';
 import 'package:dart_basic/dart_basic.dart' as dart_basic;
 
-void main(List<String> arguments) {
+void main() {
   print('Hello world: ${dart_basic.calculate()}!');
+
+  /* pesan 
+  untuk menampilkan greet
+  */
+  greet();
+
+  // print hasil hitung
+  print(hitung());
 }
---
+++ lib/M1.1.struktur-program/library.dart
@@ -0,0 +1,7 @@
+void greet() {
+  print("hello word");
+}
+
+int hitung() {
+  return 5 + 5;
+}
